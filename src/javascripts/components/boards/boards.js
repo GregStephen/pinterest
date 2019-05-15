8f4675b0f147f05024374b7131e01410b086d3e7
Greg Stephen <GregAStephen@gmail.com>
2019-05-14 19:40:46 -0500

    buttons work

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,20 +1,34 @@
 import boardData from '../../helpers/data/boardsData';
 import util from '../../helpers/util';
 
-// let boardInfo = [];
+const seePinDiv = (e) => {
+  const boardId = e.target.closest('.card').id;
+  console.error('clicked', boardId);
+  document.getElementById('boards-page').classList.add('hide');
+  document.getElementById('pins-page').classList.remove('hide');
+};
+
+const bindEvents = () => {
+  const allButtons = document.getElementsByClassName('see-pins');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seePinDiv);
+  }
+};
 
 const domStringBuilder = (boardInfo) => {
   let domString = '';
-  for (let i = 0; i < boardInfo.length; i += 1) {
+  boardInfo.forEach((board) => {
     domString += '<div class="col-3">';
-    domString += `<div id="${boardInfo[i].id}" class="card p-1">`;
+    domString += `<div id="${board.id}" class="card p-1">`;
     domString += '<div class="card-body">';
-    domString += `<h5 class="card-title">${boardInfo[i].name}</h5>`;
+    domString += `<h5 class="card-title">${board.name}</h5>`;
+    domString += '<button class="btn btn-info see-pins">Pins</button>';
     domString += '</div>';
     domString += '</div>';
     domString += '</div>';
-  }
+  });
   util.printToDom('user-boards', domString);
+  bindEvents();
 };
 
 const initBoards = () => {
@@ -25,4 +39,4 @@ const initBoards = () => {
     .catch(err => console.error('error from loadBoards', err));
 };
 
-export default { initBoards, domStringBuilder };
+export default { initBoards };
